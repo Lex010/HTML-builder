841754b2a8a2b3cbb440a4af5e8fabba45ef3c7c
Lex Mel <lex07672@gmail.com>
2024-01-17 14:07:51 +0200

    fix: task 02 mesage with exit ctrl+c

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,32 +3,31 @@ const path = require('path');
 const readline = require('readline');
 
 const filePath = path.join(__dirname, 'output.txt');
-const outputStream = fs.createWriteStream(filePath, { flags: 'a' }); // Флаг 'a' открывает файл в режиме добавления
+const outputStream = fs.createWriteStream(filePath, { flags: 'a' });
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Приветственное сообщение
 console.log('Добро пожаловать! Введите текст (введите "exit", чтобы выйти):');
 
-// Функция для обработки ввода пользователя
-function handleInput(i) {
-  if (i.toLowerCase() === 'exit') {
-    // Прощальное сообщение
-    console.log('До свидания! Выход...');
-    // Закрыть поток записи и завершить выполнение
+function handleInput(input) {
+  if (input.toLowerCase() === 'exit') {
+    console.log('До свидания! Выход.');
     outputStream.end();
     process.exit(0);
   }
 
-  // Записать введенный текст в файл
-  outputStream.write(i + '\n');
-
-  // Продолжить ожидание ввода пользователя
+  outputStream.write(input + '\n');
   rl.question('', handleInput);
 }
 
-// Начать ожидание ввода пользователя
 rl.question('', handleInput);
+
+// При выходе через Ctrl + C
+rl.on('close', () => {
+  console.log('Выход.');
+  outputStream.end();
+  process.exit(0);
+});
